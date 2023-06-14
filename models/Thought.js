22cfbea22ad12f4bcc574498f91a7b65f1a2328c
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-06-13 21:36:20 -0400

    Finished Thought Schema and added Reaction subdocument.
    Formatted with home PC prettier

--- models/Thought.js
+++ models/Thought.js
@@ -1,23 +1,69 @@
-const {Schema} = require("mongoose")
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (time) => moment(time).format("MMM, DD, YYYY [at] hh:mm a"),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            maxlength: 280,
-            minlength: 1,
-        },
-        createdAt: {
-            type: Date,
-            // Set default value to the current timestamp
-            // Set default value to the current timestamp
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        //reactions:{}
-        // Array of nested documents created with the reactionSchema
-    }
-)
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Set default value to the current timestamp
+      get: (time) => moment(time).format("MMM, DD, YYYY [at] hh:mm a"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
