58b25cd0c1d708ef01b5dda4407eb067c463cb53
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-06-13 21:35:19 -0400

    Completed the User Schema.
    Reformatted with home PC prettier.

--- models/User.js
+++ models/User.js
@@ -1,26 +1,44 @@
-const {Schema, model} = require("mongoose")
-const thoughtSchema = require("./Thought")
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trimmed: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            //valid email with mongoose validaiton
-        },
-        thoughts: [thoughtSchema],
-        //Array of _id values referencing the User model (self-reference)
-        //friends:[]
-    }
-)
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-const User = model("user", userSchema)
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
 
 module.exports = User;
