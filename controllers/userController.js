808a76a78ac972e18fec3a9ad8fb4844b7e4fe41
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-06-14 20:16:44 -0400

    Home PC prettier save

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,29 @@
-const { User, Thought } = require('../models')
+const { User, Thought } = require("../models");
 
 module.exports = {
-    getUsers(req,res) {
-        User.find().then((users) => res.json(users)).catch((err) => res.status(500).json(err));
-    },
-    getSingleUser(req,res) {
-        User.findOne({_id: req.params.userId}).populate("thoughts").populate("friends").select("-__v").then((user) => !user ? res.status(404).json({message: "No user with that ID"}) : res.json(user)).catch((err) => res.status(500).json(err));
-    },
-    createUser(req,res) {
-        User.create(req.body).then((user) => res.json(user)).catch((err) => {console.log(err); return res.status(500).json(err)});
-    },
-}
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+};
